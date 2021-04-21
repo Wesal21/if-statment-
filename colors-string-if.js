fc222066c33a88b13edda6fa19dc7b1165a7979e
Eng.Wesal <eng-wesal@eng.wesal-HP-250-G7-Notebook-PC>
2021-04-22 00:35:39 +0300

    if statment with string

---
+++ colors-string-if.js
@@ -0,0 +1,57 @@
+
+  {
+    let color = prompt("Enter a color");
+    
+   
+    
+    if(color==="red"){
+        alert("apple");
+      }
+        
+      if(color==="yellow"){
+        alert("banana");}
+        
+      if(color==="green"){
+        alert("grass");
+      }
+
+      if(color==="rectangle"){
+        alert("shape");
+      }
+        
+      if(color==="square"){
+        alert("shape");}
+        
+      if(color===color.toString()){
+       alert("i don't know");
+      }
+      
+  
+
+  }
+
+      
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+         
+    
+ 
+   
+ 
+ 
+    
